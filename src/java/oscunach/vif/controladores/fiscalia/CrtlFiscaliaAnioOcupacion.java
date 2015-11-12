e629d19214b6b68338871c195e755793a192cd18
Oscunach <Oscunach@Oscunach-PC>
2015-11-12 12:43:10 -0500

    varios

--- src/java/oscunach/vif/controladores/fiscalia/CrtlFiscaliaAnioOcupacion.java
+++ src/java/oscunach/vif/controladores/fiscalia/CrtlFiscaliaAnioOcupacion.java
@@ -13,7 +13,7 @@ import org.primefaces.model.chart.CartesianChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import oscunach.vif.busquedas.FBFichaFiscalia;
 import oscunach.vif.entidades.FichaVif;
-
+import recursos.Ocupaciones;
 
 /**
  *
@@ -23,10 +23,27 @@ import oscunach.vif.entidades.FichaVif;
 @RequestScoped
 public class CrtlFiscaliaAnioOcupacion {
 
-     private ArrayList<FichaVif>lst;
+    private ArrayList<FichaVif> lst;
+    private ArrayList<String> lst1;
     private int aniosel;
     private CartesianChartModel graficarOcupacion;
     private CartesianChartModel graficarOcupacioGewero;
+
+    public ArrayList<String> getLst1() {
+        return lst1;
+    }
+
+    public void setLst1(ArrayList<String> lst1) {
+        this.lst1 = lst1;
+    }
+
+    public CartesianChartModel getGraficarOcupacion() {
+        return graficarOcupacion;
+    }
+
+    public void setGraficarOcupacion(CartesianChartModel graficarOcupacion) {
+        this.graficarOcupacion = graficarOcupacion;
+    }
 
     public ArrayList<FichaVif> getLst() {
         return lst;
@@ -59,28 +76,27 @@ public class CrtlFiscaliaAnioOcupacion {
     public void setGraficarOcupacioGewero(CartesianChartModel graficarOcupacioGewero) {
         this.graficarOcupacioGewero = graficarOcupacioGewero;
     }
-    
+
     public CrtlFiscaliaAnioOcupacion() {
         this.graficar();
     }
 
-     @PostConstruct
-     
-      public void graficar() {
+    @PostConstruct
+
+    public void graficar() {
         this.graficarOcupacion = ocupaciones(aniosel);
         this.graficarOcupacioGewero = ocupacionesGenero(aniosel);
     }
-        
-    
-    
-     private CartesianChartModel ocupaciones(int anio) {
+
+    private CartesianChartModel ocupaciones(int anio) {
         CartesianChartModel model = new CartesianChartModel();
         try {
-            lst = FBFichaFiscalia.obtenerOcupacionesDadoAnio(anio);
+           // lst = FBFichaFiscalia.obtenerOcupacionesDadoAnio(anio);
+            lst1 = Ocupaciones.enlistarOcupaciones();
             ChartSeries ocupaciones = new ChartSeries();
             ocupaciones.setLabel("Ocupaciones");
-            for (int i = 0; i < lst.size(); i++) {
-                ocupaciones.set(lst.get(i).getOcupacion_victima(), FBFichaFiscalia.obtenerFichaDadoAnioOcupacion(anio, lst.get(i).getOcupacion_victima()).size());
+            for (int i = 0; i < lst1.size(); i++) {
+                ocupaciones.set(lst1.get(i), FBFichaFiscalia.obtenerFichaDadoAnioOcupacion(anio, lst1.get(i)).size());
             }
             model.addSeries(ocupaciones);
         } catch (Exception e) {
@@ -92,17 +108,19 @@ public class CrtlFiscaliaAnioOcupacion {
     private CartesianChartModel ocupacionesGenero(int anio) {
         CartesianChartModel model = new CartesianChartModel();
         try {
-            lst = lst = FBFichaFiscalia.obtenerOcupacionesDadoAnio(anio);
+            //lst = FBFichaFiscalia.obtenerOcupacionesDadoAnio(anio);
+            lst1 = Ocupaciones.enlistarOcupaciones();
             ChartSeries masculino = new ChartSeries();
             masculino.setLabel("Masculino");
-            for (int i = 0; i < lst.size(); i++) {
-                masculino.set(lst.get(i).getOcupacion_victima(),FBFichaFiscalia.obtenerFichaDadoAnioOcupacionGenero(anio, lst.get(i).getOcupacion_victima(), "Masculino") .size());
+            for (int i = 0; i < lst1.size(); i++) {
+                masculino.set(lst1.get(i), FBFichaFiscalia.obtenerFichaDadoAnioOcupacionGenero(anio, lst1.get(i), "Masculino").size());
             }
             ChartSeries femenino = new ChartSeries();
-            femenino.setLabel("Masculino");
-            for (int i = 0; i < lst.size(); i++) {
-                 femenino.set(lst.get(i).getOcupacion_victima(),FBFichaFiscalia.obtenerFichaDadoAnioOcupacionGenero(anio, lst.get(i).getOcupacion_victima(), "FEMENINO") .size());
-              }
+            femenino.setLabel("Femenino");
+            for (int i = 0; i < lst1.size(); i++) {
+                femenino.set(lst1.get(i), FBFichaFiscalia.obtenerFichaDadoAnioOcupacionGenero(anio, lst1.get(i), "Femenino").size());
+            }
+
             model.addSeries(femenino);
             model.addSeries(masculino);
         } catch (Exception e) {
@@ -111,4 +129,3 @@ public class CrtlFiscaliaAnioOcupacion {
         return model;
     }
 }
-
